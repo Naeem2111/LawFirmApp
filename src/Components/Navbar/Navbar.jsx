847f5d89b5bc5013d7a6e4dc949b8787e5c1bf1f
nthomascpt <nthomas@student.wethinkcode.co.za>
2021-10-01 14:44:02 +0200

    Navbar+ Navbar scss

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,12 @@
+import React from "react"
+import "./Navbar.scss";
+
 const Navbar = () => {
-  return <div></div>
+  return <div>
+    <div class="navbar">
+        <div class="logo">Bank Name</div>
+    </div>
+  </div>
 }
 
 export default Navbar
