cb4dea798e66a280795f4e3162ea25d7855c1082
nthomascpt <nthomas@student.wethinkcode.co.za>
2022-02-06 05:26:22 +0200

    Assessment screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home'
 import Risktables from './Pages/RiskTables/Risktables'
 import IRA from './Pages/IRA/IRA'
 import CustomerRisk from './Pages/IRA/CustomerRisk/CustomerRisk'
+import Assessment from './Pages/Assessment/Assessment'
 const App = () => {
   return (
     <div>
@@ -25,6 +26,9 @@ const App = () => {
           </Route>
           <Route exact path="/customer-risk">
             <CustomerRisk />
+          </Route>
+          <Route exact path="/assessment">
+            <Assessment />
           </Route>
         </Switch>
       </Router>
